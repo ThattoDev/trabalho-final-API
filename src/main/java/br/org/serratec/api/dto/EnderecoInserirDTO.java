dd2fa1cc91bfd31bdc00064d18f08a35de11150f
Thatto <thacito.pfilgueiras@gmail.com>
2022-06-03 14:54:58 -0300

    Update

---
+++ src/main/java/br/org/serratec/api/dto/EnderecoInserirDTO.java
@@ -0,0 +1,29 @@
+package br.org.serratec.api.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import br.org.serratec.api.model.Endereco;
+
+public class EnderecoInserirDTO {
+
+	@NotBlank
+	@Size(max = 9)
+	private String cep;
+
+	public EnderecoInserirDTO() {
+
+	}
+
+	public EnderecoInserirDTO(Endereco endereco) {
+		this.cep = endereco.getCep();
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+}
--- src/main/java/br/org/serratec/api/model/Endereco.java
+++ src/main/java/br/org/serratec/api/model/Endereco.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(schema = "")
 public class Endereco {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
